ad34b0529f366acf3b3a52b572454d5f7276ec8a
croberttdmello <eng.carlosrobert@gmail.com>
2020-04-26 20:42:46 -0300

    Updates

---
+++ precache-manifest.dad16a1d55cb6913ebd776e3d39f72d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68f844f0e2b7c556fe8d4cdc2cecc9a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "de95014259b0cf71b2e6",
+    "url": "/monsters-rolodex/static/css/main.d2905037.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de95014259b0cf71b2e6",
+    "url": "/monsters-rolodex/static/js/main.4adc06d9.chunk.js"
+  },
+  {
+    "revision": "0c81cb278443448182f0",
+    "url": "/monsters-rolodex/static/js/runtime-main.165419b0.js"
+  }
+]);
